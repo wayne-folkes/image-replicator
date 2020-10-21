cd5c9b41bb262cfc39ad50d415ec5557362e0f68
Wayne <wayne.folkes@gmail.com>
2020-10-21 03:58:04 +0000

    linter, linter on the wall

--- pull-push.sh
+++ pull-push.sh
@@ -7,7 +7,7 @@ echo The source is "${SOURCE}" the target is "${DESTINATION}"
 
 docker pull "${SOURCE}" && \
 docker tag "${SOURCE}" "${DESTINATION}" && \
-eval $(aws ecr get-login --no-include-email --region us-east-1) && \
+eval "$(aws ecr get-login --no-include-email --region us-east-1)" && \
 docker push "${DESTINATION}"
 
 echo "${DESTINATION}" pushed successfully
